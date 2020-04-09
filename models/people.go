646c79083141a970c1556dbc9b4a6f241bbd15dc
Lukas Danin <lukas.danin@gmail.com>
2020-04-08 22:11:01 -0300

    Exports type people » People

--- models/people.go
+++ models/people.go
@@ -5,10 +5,11 @@ import (
 	"time"
 )
 
-type people []Person
+// People is a group of Person
+type People []Person
 
 // MakePeople returns a slice full of Person, it will never be empty
-func MakePeople() (result people) {
+func MakePeople() (result People) {
 	min := 5
 	max := 15
 
@@ -25,7 +26,7 @@ func MakePeople() (result people) {
 	return result
 }
 
-func (p people) GenerateNoise() {
+func (p People) GenerateNoise() {
 	for i := range p {
 		p[i].Coordinates.Noise()
 	}
